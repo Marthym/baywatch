699453a520d4ed8e0871b93f3255dc305c57ba11
fcombes <frederic.combes@i-run.fr>
2020-12-21 15:27:58 +0100

    test(infra): Fix RecordToFeedConverterTest

--- src/test/java/fr/ght1pc9kc/baywatch/infra/mappers/RecordToFeedConverterTest.java
+++ src/test/java/fr/ght1pc9kc/baywatch/infra/mappers/RecordToFeedConverterTest.java
@@ -3,14 +3,23 @@ package fr.ght1pc9kc.baywatch.infra.mappers;
 import fr.ght1pc9kc.baywatch.api.model.Feed;
 import fr.ght1pc9kc.baywatch.api.model.RawFeed;
 import fr.ght1pc9kc.baywatch.domain.utils.Hasher;
+import org.jooq.Field;
+import org.jooq.Record;
+import org.jooq.impl.DSL;
+import org.jooq.impl.DefaultConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.convert.support.DefaultConversionService;
 
 import java.net.URI;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 import static fr.ght1pc9kc.baywatch.dsl.tables.Feeds.FEEDS;
+import static fr.ght1pc9kc.baywatch.dsl.tables.FeedsUsers.FEEDS_USERS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RecordToFeedConverterTest {
@@ -29,18 +38,24 @@ class RecordToFeedConverterTest {
 
     @Test
     void should_convert_FeedRecord_to_Feed_pojo() {
-        Feed actual = tested.convert(FEEDS.newRecord()
-                .setFeedId(TEST_SHA3)
-                .setFeedName("Blog ght1pc9kc")
-                .setFeedUrl(TEST_URL.toString())
-                .setFeedLastWatch(DateUtils.toLocalDateTime(PUBLICATION))
-        );
+        List<Field<?>> fields = new ArrayList<>(Arrays.asList(FEEDS.fields()));
+        fields.add(FEEDS_USERS.FEUS_TAGS);
+
+        Record record = DSL.using(new DefaultConfiguration()).newRecord(fields);
+        record.set(FEEDS.FEED_ID, TEST_SHA3);
+        record.set(FEEDS.FEED_NAME, "Blog ght1pc9kc");
+        record.set(FEEDS.FEED_URL, TEST_URL.toString());
+        record.set(FEEDS.FEED_LAST_WATCH, DateUtils.toLocalDateTime(PUBLICATION));
+        record.set(FEEDS_USERS.FEUS_TAGS, "jedi,light");
+
+        Feed actual = tested.convert(record);
 
         assertThat(actual).isEqualTo(Feed.builder().raw(RawFeed.builder()
                 .id(TEST_SHA3)
                 .name("Blog ght1pc9kc")
                 .lastWatch(PUBLICATION)
                 .url(TEST_URL)
-                .build()).build());
+                .build())
+                .tags(Set.of("jedi", "light")).build());
     }
 }
