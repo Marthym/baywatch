df6b4d9f1e9073530620e9b6423475b158279bc3
Marthym <marthym@gmail.com>
2021-02-07 23:36:23 +0100

    feat(seaside): Add optional query string for getNews
    This allow to require only some pages. By default api require the
    10 latest published news

--- seaside/src/services/NewsService.ts
+++ seaside/src/services/NewsService.ts
@@ -4,14 +4,25 @@ import {switchMap} from "rxjs/operators";
 import {HttpStatusError} from "@/services/model/exceptions/HttpStatusError";
 
 export default class NewsService {
+    /**
+     * By default query the 10 latest published news
+     * @private
+     */
+    private static readonly DEFAULT_QUERY: URLSearchParams = new URLSearchParams("?_pp=10&_s=publication");
+
     serviceBaseUrl: string;
 
     constructor(baseURL: string) {
         this.serviceBaseUrl = baseURL;
     }
 
-    getNews(): Observable<Array<News>> {
-        return fromFetch(`${this.serviceBaseUrl}/news`).pipe(
+    /**
+     * Get the {@link News} from backend
+     *
+     * @param query
+     */
+    getNews(query: URLSearchParams = NewsService.DEFAULT_QUERY): Observable<Array<News>> {
+        return fromFetch(`${this.serviceBaseUrl}/news?${query.toString()}`).pipe(
             switchMap(response => {
                 if (response.ok) {
                     return response.json();
