8e19e02e282b2928b0569fb28d90f6c5e6027a9f
Marthym <marthym@gmail.com>
2021-02-21 22:43:36 +0100

    wip(seaside): Add User login

--- seaside/src/components/sidenav/SideNavUserInfo.vue
+++ seaside/src/components/sidenav/SideNavUserInfo.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="mt-8">
     <!-- User info -->
-    <img
-        class="h-12 w-12 rounded-full object-cover"
-        src="https://appzzang.me/data/editor/1608/f9c387cb6bd7a0b004f975cd92cbe2d9_1471626325_6802.png"
-        alt="enoshima profile"/>
-    <h2
-        class="mt-4 text-xl dark:text-gray-300 font-extrabold capitalize">
+    <img class="h-12 w-12 rounded-full object-cover"
+         src="https://appzzang.me/data/editor/1608/f9c387cb6bd7a0b004f975cd92cbe2d9_1471626325_6802.png"
+         alt="enoshima profile"/>
+    <h2 class="mt-4 text-xl dark:text-gray-300 font-extrabold capitalize">
       Hello Enoshima
     </h2>
     <span class="text-sm dark:text-gray-300">
-				<span class="font-semibold text-green-600 dark:text-green-300">
-					Admin
-				</span>
-				id789038
-			</span>
+				<span class="font-semibold text-green-600 dark:text-green-300">Admin</span>
+      id789038
+    </span>
   </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
+import {Component, Prop, Vue} from 'vue-property-decorator';
+import {User} from "@/services/model/User";
 
 @Component
 export default class SideNavUserInfo extends Vue {
+  @Prop user?: User;
 }
 </script>
---
+++ seaside/src/services/model/User.ts
@@ -0,0 +1,6 @@
+export type User = {
+    id: string;
+    login: string;
+    name: string;
+    mail: string;
+}
