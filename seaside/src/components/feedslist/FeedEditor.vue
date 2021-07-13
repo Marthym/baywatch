9e30ea02495bff55d31e9fdfdf2e88faf878c7ed
Marthym <marthym@gmail.com>
2021-07-13 23:47:29 +0200

    feat(seaside): #22 implment tag input for feeds
    refs #22

--- seaside/src/components/feedslist/FeedEditor.vue
+++ seaside/src/components/feedslist/FeedEditor.vue
@@ -9,10 +9,11 @@
         <span class="label-text">URL</span>
       </label>
       <input type="text" placeholder="username" class="input input-bordered">
+      <TagInput/>
     </form>
     <template v-slot:actions>
       <button class="btn btn-primary">Enregistrer</button>
-      <button class="btn" @click.stop="isOpened = false">Fermer</button>
+      <button class="btn" @click.stop="isOpened = false">Annuler</button>
     </template>
   </ModalWindow>
 </template>
@@ -22,9 +23,11 @@ import {Component, Vue} from 'vue-property-decorator';
 import {Feed} from '@/services/model/Feed';
 import {Observable} from "rxjs";
 import ModalWindow from "@/components/shared/ModalWindow.vue";
+import TagInput from "@/components/shared/TagInput.vue";
 
 @Component({
   components: {
+    TagInput,
     ModalWindow,
   },
 })
@@ -32,6 +35,9 @@ export default class FeedEditor extends Vue {
   private feed!: Feed;
   private isOpened = false;
   private modalTitle = 'Ajouter un fil';
+
+  private tag = '';
+  private tags: string[] = [];
 
   public openEmpty(): Observable<Feed> {
     this.isOpened = true;
---
+++ seaside/src/components/shared/TagInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-row items-center flex-wrap mt-2">
+    <div v-for="tag in tags" :key="tag"
+         class="badge badge-primary m-1">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+           class="inline-block w-4 h-4 mr-2 stroke-current">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+      </svg>
+      {{ tag }}
+    </div>
+    <input type="text" placeholder="Ajouter un tag..." class="input input-ghost input-xs w-32"
+           v-model.trim="tag" @keydown="onKeydown">
+  </div>
+</template>
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class TagInput extends Vue {
+  private tags: string[] = ['test', 'info'];
+  private tag = '';
+
+  private onKeydown(event: KeyboardEvent): void {
+    switch (event.key) {
+      case "Enter":
+        if (this.tags.indexOf(this.tag) === -1) {
+          this.tags.push(this.tag);
+        }
+        this.tag = '';
+    }
+  }
+}
+</script>
