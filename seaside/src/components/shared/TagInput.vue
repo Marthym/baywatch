2adcb8a8be84629fa316ad84f2ad1cb55cc74dfe
Marthym <marthym@gmail.com>
2021-07-14 12:40:19 +0200

    feat(seaside): #22 Manage v-model for TagInput
    refs #22

--- seaside/src/components/shared/TagInput.vue
+++ seaside/src/components/shared/TagInput.vue
@@ -3,10 +3,12 @@
     <div v-for="tag in tags" :key="tag.name"
          class="badge m-1"
          :class="tag.status">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-           class="inline-block w-4 h-4 mr-2 stroke-current">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-      </svg>
+      <button type="button" @click="onRemoveTag(tag)" class="p-0 m-0">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+             class="inline-block w-4 h-4 mr-2 mb-0.5 stroke-current">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+        </svg>
+      </button>
       {{ tag.name }}
     </div>
     <input type="text" placeholder="Ajouter un tag..." class="input input-ghost input-xs w-32 flex-grow"
@@ -14,17 +16,27 @@
   </div>
 </template>
 <script lang="ts">
-import {Component, Vue} from "vue-property-decorator";
+import {Component, Prop, Vue} from "vue-property-decorator";
 
 @Component({
   components: {},
 })
 export default class TagInput extends Vue {
-  private tags: TagView[] = [
-    {name: 'test', status: TagStatus.PRIMARY},
-    {name: 'info', status: TagStatus.PRIMARY},
-  ];
+  @Prop({default: []}) private value!: string[];
+
   private tag = '';
+  private tags: TagView[] = [];
+
+  mounted(): void {
+    this.tags = this.value.map(v => ({name: v, status: TagStatus.PRIMARY}));
+  }
+
+  private onRemoveTag(tv: TagView) {
+    console.log('remove', tv);
+    const idx = this.tags.indexOf(tv);
+    this.tags.splice(idx, 1);
+    this.emitInputEvent();
+  }
 
   private onKeydown(event: KeyboardEvent): void {
     switch (event.key) {
@@ -33,6 +45,7 @@ export default class TagInput extends Vue {
           this.tags.push({name: this.tag, status: TagStatus.PRIMARY});
         }
         this.tag = '';
+        this.emitInputEvent();
         break;
       case 'Backspace':
         if (this.tag === '' && this.tags.length > 0) {
@@ -43,12 +56,26 @@ export default class TagInput extends Vue {
             if (lastTag.timeout) {
               clearTimeout(lastTag.timeout);
             }
+            this.emitInputEvent();
           } else {
             lastTag.status = TagStatus.ALERT;
             lastTag.timeout = setTimeout(() => lastTag.status = TagStatus.PRIMARY, 1000);
           }
         }
+        break;
     }
+  }
+
+  private beforeUnmount(): void {
+    this.tags.forEach(tv => {
+      if (tv.timeout) {
+        clearTimeout(tv.timeout);
+      }
+    })
+  }
+
+  private emitInputEvent(): void {
+    this.$emit('input', this.tags.map(tv => tv.name));
   }
 }
 
