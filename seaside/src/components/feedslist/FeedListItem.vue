a2cabb4b3a3b2c8b0109e3e2fa181108a0e1d8e5
Marthym <marthym@gmail.com>
2021-07-30 00:20:55 +0200

    feat(seaside): #25 add update and delete event on FeedListItem
    refs #25

--- seaside/src/components/feedslist/FeedListItem.vue
+++ seaside/src/components/feedslist/FeedListItem.vue
@@ -33,14 +33,16 @@
     </td>
     <th v-if="isAuthenticated">
       <div class="btn-group">
-        <button class="btn btn-sm btn-square btn-ghost">
+        <button class="btn btn-sm btn-square btn-ghost" @click="$emit('item-update', view.data)">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/>
           </svg>
         </button>
-        <button class="btn btn-sm btn-square btn-ghost">
+        <button class="btn btn-sm btn-square btn-ghost" @click="$emit('item-delete', view.data.id)">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
           </svg>
         </button>
       </div>
