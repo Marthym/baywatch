e15341e1e354dbe6ad00909f1f741fd546d1ba3b
Marthym <marthym@gmail.com>
2022-01-03 17:38:42 +0100

    fix(seaside): #54 improve inactive card and lighten html code

--- seaside/src/components/newslist/NewsCard.vue
+++ seaside/src/components/newslist/NewsCard.vue
@@ -2,44 +2,33 @@
   <div class="flex flex-col lg:flex-row bg-white dark:bg-gray-600 shadow rounded-lg"
        :class="{
         'shadow-lg lg:h-60 my-8 border border-gray-400': card.isActive,
-        'lg:h-56 m-5': !card.isActive
+        'lg:h-56 m-5': !card.isActive,
+        'opacity-30': card.data.read && !card.isActive,
        }" @click="$emit('activate')">
 
     <figure class="flex-none">
       <img v-if="card.data.image"
            :src="card.data.image"
            class="w-full h-24 lg:h-full lg:w-60 object-cover rounded-t-lg lg:rounded-none lg:rounded-l-lg"
-           :class="{ 'opacity-30': card.data.read }"
-           alt="og:image"/>
+           :alt="card.data.title"/>
       <img v-else
            class="w-full h-24 lg:w-60 lg:h-full object-contain rounded-t-lg lg:rounded-none lg:rounded-l-lg"
-           :class="{ 'opacity-30': card.data.read }"
            src="/placeholder.png"
-           alt="no-og-image"/>
+           :alt="card.data.title"/>
     </figure>
 
     <div :class="{ 'm-6': card.isActive, 'm-4': !card.isActive}" class="flex-grow">
-      <div class="flex flex-col h-full"
-           :class="{
-              'text-black': !card.data.read || card.isActive,
-              'text-gray-400': card.data.read && !card.isActive,
-              'dark:text-gray-200': !card.data.read || card.isActive,
-              'dark:text-gray-500': card.data.read && !card.isActive
-            }">
+      <div class="flex flex-col h-full">
         <a class="font-semibold text-xl" :href="card.data.link" :title="card.data.link" v-html="card.data.title"></a>
-        <span v-bind:class="{
-                'text-gray-600': !card.data.read || card.isActive,
-                'text-gray-300': card.data.read && !card.isActive,
-                'dark:text-gray-300': !card.data.read || card.isActive,
-                'dark:text-gray-500': card.data.read && !card.isActive,
-              }"
-              v-html="card.data.description" class="mt-2 text-base flex-grow overflow-hidden"></span>
+        <span v-html="card.data.description" class="mt-2 text-base flex-grow overflow-hidden"></span>
 
         <div class="flex flex-row-reverse text-sm italic mt-1">
           <span>{{ card.data.publication }}</span>
         </div>
-        <div class="flex flex-row-reverse text-xs italic" v-bind:class="{'-mb-2': card.isActive}">
-          <span>{{ card.feeds.join(' | ') }}</span>
+        <div class="flex flex-row-reverse italic">
+          <div class="badge badge-xs p-2"
+               v-for="f in card.feeds">{{ f }}
+          </div>
           <span class="flex-grow"></span>
           <slot name="actions"></slot>
         </div>
