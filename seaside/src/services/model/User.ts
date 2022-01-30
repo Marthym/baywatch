04681d6d9c5c733cf0734d5a043209a3f272f40a
Marthym <marthym@gmail.com>
2022-01-30 19:34:44 +0100

    feat(seaside): #72 add role into user information
    and use it to display some administrative feature
    
    close #72

--- seaside/src/services/model/User.ts
+++ seaside/src/services/model/User.ts
@@ -1,6 +1,17 @@
+import {UserRole} from "@/services/model/UserRole.enum";
+
 export type User = {
     id: string;
     login: string;
     name: string;
     mail: string;
+    role: UserRole;
 }
+
+export const ANONYMOUS: User = {
+    id: '',
+    login: '',
+    name: 'Anonymous',
+    mail: '',
+    role: UserRole.ANONYMOUS,
+};
---
+++ seaside/src/services/model/UserRole.enum.ts
@@ -0,0 +1,7 @@
+export enum UserRole {
+    SYSTEM= "SYSTEM",
+    ADMIN = "ADMIN",
+    MANAGER = "MANAGER",
+    USER = "USER",
+    ANONYMOUS = "ANONYMOUS"
+}
