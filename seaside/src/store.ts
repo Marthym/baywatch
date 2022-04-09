6f563b501f5046268ed1dfcb7fc44c0cbd5c03b9
Marthym <marthym@gmail.com>
2022-04-09 21:46:20 +0200

    feat(seaside): #93 add store for news filters
    refs #93

--- seaside/src/store.ts
+++ seaside/src/store.ts
@@ -2,11 +2,13 @@ import {createLogger, createStore} from "vuex";
 import sidenav from '@/store/sidenav/sidenav'
 import statistics from '@/store/statistics/statistics'
 import user from '@/store/user/user'
+import news from '@/store/news/news'
 
 const debug = process.env.NODE_ENV !== 'production'
 
 export default createStore({
     modules: {
+        news,
         sidenav,
         statistics,
         user,
