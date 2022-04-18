b5e5c6984ca096749ec6c4f05ef8b07ed5ed8017
Marthym <marthym@gmail.com>
2022-04-18 16:24:13 +0200

    feat(seaside): open news in news tabs

--- seaside/src/components/newslist/NewsCard.vue
+++ seaside/src/components/newslist/NewsCard.vue
@@ -19,7 +19,7 @@
 
     <div :class="{ 'm-6': card.isActive, 'm-4': !card.isActive}" class="flex-grow">
       <div class="flex flex-col h-full">
-        <a class="font-semibold text-xl" :href="card.data.link" :title="card.data.link" v-html="card.data.title"></a>
+        <a class="font-semibold text-xl" target="_blank" :href="card.data.link" :title="card.data.link" v-html="card.data.title"></a>
         <span v-html="card.data.description" class="mt-2 text-base flex-grow overflow-hidden"></span>
 
         <div class="flex flex-row justify-end items-end text-xs">
