ee25e51f79bcb2e7ae8f2538777f5bb39225b0e1
fcombes <frederic.combes@i-run.fr>
2022-05-05 14:17:15 +0200

    feat(sandside): ght1pc9kc.fr/scraphead#10 improve WebClient configuration
    refs ght1pc9kc.fr/scraphead#10

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/scrapper/infra/config/WebClientConfiguration.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/scrapper/infra/config/WebClientConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.Http11SslContextSpec;
 import reactor.netty.http.client.HttpClient;
 
 import java.net.InetSocketAddress;
@@ -39,16 +40,16 @@ public class WebClientConfiguration {
     @Bean
     @ScraperQualifier
     public WebClient getScraperWebClient(ScraperProperties properties, DnsAddressResolverGroup dnsAddressResolverGroup) {
-        return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(
-                        HttpClient.create()
-                                .resolver(dnsAddressResolverGroup)
-                                .followRedirect(true)
-                                .followRedirect((req, res) -> // 303 was not in the default code
-                                        Set.of(301, 302, 303, 307, 308).contains(res.status().code()))
-                                .compress(true)
-                                .responseTimeout(properties.timeout())
-                                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) properties.timeout().toMillis())
-                )).build();
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(
+                HttpClient.create()
+                        .resolver(dnsAddressResolverGroup)
+                        .followRedirect(true)
+                        .followRedirect((req, res) -> // 303 was not in the default code
+                                Set.of(301, 302, 303, 307, 308).contains(res.status().code()))
+                        .compress(true)
+                        .secure(spec -> spec.sslContext(Http11SslContextSpec.forClient()))
+                        .responseTimeout(properties.timeout())
+                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) properties.timeout().toMillis())
+        )).build();
     }
 }
