dee02e4026eea89430cc9d14dceb94e93bd77dd9
Marthym <marthym@gmail.com>
2022-05-07 15:12:21 +0200

    fix(sandside): #114 avoid override dns servers
    Override DNS server cause error at startup only in production
    java.lang.IllegalStateException: failed to create a new resolver
    
    refs #113
    fix #114

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/scrapper/infra/config/WebClientConfiguration.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/scrapper/infra/config/WebClientConfiguration.java
@@ -1,10 +1,6 @@
 package fr.ght1pc9kc.baywatch.scrapper.infra.config;
 
 import io.netty.channel.ChannelOption;
-import io.netty.channel.epoll.EpollDatagramChannel;
-import io.netty.resolver.dns.DnsAddressResolverGroup;
-import io.netty.resolver.dns.DnsNameResolverBuilder;
-import io.netty.resolver.dns.SequentialDnsServerAddressStreamProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -12,8 +8,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.Http11SslContextSpec;
 import reactor.netty.http.client.HttpClient;
 
-import java.net.InetSocketAddress;
-import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -21,28 +15,10 @@ public class WebClientConfiguration {
 
     @Bean
     @ScraperQualifier
-    public DnsAddressResolverGroup getDnsAddressResolver(ScraperProperties properties) {
-        var dnsNameResolverBuilder = new DnsNameResolverBuilder()
-                .queryTimeoutMillis(properties.dns().timeout().toMillis())
-                .channelType(EpollDatagramChannel.class);
-
-        List<InetSocketAddress> servers = properties.dns().servers().stream()
-                .map(ip -> new InetSocketAddress(ip, 53))
-                .toList();
-
-        if (!servers.isEmpty()) {
-            dnsNameResolverBuilder.nameServerProvider(new SequentialDnsServerAddressStreamProvider(servers));
-        }
-
-        return new DnsAddressResolverGroup(dnsNameResolverBuilder);
-    }
-
-    @Bean
-    @ScraperQualifier
-    public WebClient getScraperWebClient(ScraperProperties properties, DnsAddressResolverGroup dnsAddressResolverGroup) {
+    public WebClient getScraperWebClient(ScraperProperties properties) {
         return WebClient.builder().clientConnector(new ReactorClientHttpConnector(
                 HttpClient.create()
-                        .resolver(dnsAddressResolverGroup)
+                        .resolver(spec -> spec.queryTimeout(properties.dns().timeout()))
                         .followRedirect(true)
                         .followRedirect((req, res) -> // 303 was not in the default code
                                 Set.of(301, 302, 303, 307, 308).contains(res.status().code()))
