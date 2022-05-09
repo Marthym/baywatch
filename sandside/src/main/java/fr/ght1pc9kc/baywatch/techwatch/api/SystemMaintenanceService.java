fb4b1e501d04be4732a4924a7ed7d96e3530b84c
fcombes <frederic.combes@i-run.fr>
2022-05-09 17:44:05 +0200

    refactor(sandside): #51 add SystemMaintenanceService
    In replacement of FeedAdminService in order to perfom
    maintenance operations by SYSTEM user
    move test for SystemMaintenanceService
    
    refs #51

---
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/techwatch/api/SystemMaintenanceService.java
@@ -0,0 +1,58 @@
+package fr.ght1pc9kc.baywatch.techwatch.api;
+
+import fr.ght1pc9kc.baywatch.security.api.model.User;
+import fr.ght1pc9kc.baywatch.techwatch.api.model.Feed;
+import fr.ght1pc9kc.baywatch.techwatch.api.model.News;
+import fr.ght1pc9kc.baywatch.techwatch.api.model.RawFeed;
+import fr.ght1pc9kc.baywatch.techwatch.api.model.RawNews;
+import fr.ght1pc9kc.baywatch.techwatch.api.model.State;
+import fr.ght1pc9kc.juery.api.PageRequest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+
+public interface SystemMaintenanceService {
+    /**
+     * List {@link RawFeed} independently of the {@link User} or any other entity
+     *
+     * @return The {@link RawFeed} version of the feed
+     */
+    Flux<RawFeed> feedList();
+
+    /**
+     * List {@link RawFeed} independently of the {@link User} or any other entity.
+     *
+     * @param pageRequest The query parameters
+     * @return The {@link RawFeed} version of the feed
+     */
+    Flux<RawFeed> feedList(PageRequest pageRequest);
+
+    Mono<Integer> feedDelete(Collection<String> toDelete);
+
+    /**
+     * List {@link RawNews} for connected user or {@link RawNews} for anonymous.
+     * For Anonymous, {@link State} is always
+     * {@link State#NONE}
+     *
+     * @param pageRequest {@see PageRequest}
+     * @return The {@link RawNews} for connected user or {@link RawNews} for anonymous
+     */
+    Flux<RawNews> newsList(PageRequest pageRequest);
+
+    /**
+     * Delete all links between {@link News} and {@link Feed}
+     *
+     * @param toOrphanize The {@link News} to be orphans
+     * @return nothing
+     */
+    Mono<Integer> newsOrphanize(Collection<String> toOrphanize);
+
+    /**
+     * Delete {@link RawNews} from the database
+     *
+     * @param toDelete ID of the {@link RawNews}s to delete
+     * @return The number of {@link RawNews} effectively deleted
+     */
+    Mono<Integer> newsDelete(Collection<String> toDelete);
+}
