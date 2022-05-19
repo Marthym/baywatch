7724c024a3333e4d2d397c634ed2af3dbba36083
fcombes <frederic.combes@i-run.fr>
2022-05-19 13:53:34 +0200

    feat(seaside): #122 refactor statistics service et display news counters
    add heroicon library for dynamic icon
    
    close #122

--- seaside/src/administration/services/StatisticsService.ts
+++ seaside/src/administration/services/StatisticsService.ts
@@ -1,15 +1,15 @@
 import {switchMap, take} from "rxjs/operators";
 import {HttpStatusError} from "@/services/model/exceptions/HttpStatusError";
-import {Statistics} from "@/administration/model/Statistics.type";
 import rest from '@/services/http/RestWrapper';
 import {Observable} from "rxjs";
+import {Counter} from "@/administration/model/Counter.type";
 
 export class StatisticsService {
-    get(): Observable<Statistics> {
+    get(): Observable<Counter[]> {
         return rest.get('/stats').pipe(
             switchMap(response => {
                 if (response.ok) {
-                    return response.json() as Promise<Statistics>;
+                    return response.json() as Promise<Counter[]>;
                 } else {
                     throw new HttpStatusError(response.status, `Error while getting news.`);
                 }
