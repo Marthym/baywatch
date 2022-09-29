9399459c5995f2572de9ab21c923c26d47f903f4
Marthym <marthym@gmail.com>
2022-09-29 22:58:34 +0200

    chore(seaside): #133 replace svg by heroicon
    refs #133

--- seaside/src/layout/components/sidenav/SideNavFilters.vue
+++ seaside/src/layout/components/sidenav/SideNavFilters.vue
@@ -2,12 +2,7 @@
   <ul class="pt-8">
     <li>
       <div class="flex">
-        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd"
-                d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0
-                018 17v-5.586L3.293 6.707A1 1 0 013 6V3z"
-                clip-rule="evenodd"></path>
-        </svg>
+        <FunnelIcon class="w-6 h-6"/>
         <span class="ml-2 capitalize font-medium">filters</span>
       </div>
 
@@ -30,10 +25,7 @@
     </li>
     <li class="mt-2">
       <div class="flex">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"/>
-        </svg>
+        <TagIcon class="h-6 w-6"/>
         <span class="ml-2 capitalize font-medium">tags</span>
       </div>
 
@@ -71,9 +63,9 @@ import {
   NEWS_TOGGLE_UNREAD_MUTATION,
   NewsStore
 } from "@/common/model/store/NewsStore.type";
-import {XMarkIcon} from "@heroicons/vue/24/outline";
+import {FunnelIcon, TagIcon, XMarkIcon} from "@heroicons/vue/24/outline";
 
-@Options({name: 'SideNavFilters', components: {XMarkIcon}})
+@Options({name: 'SideNavFilters', components: {FunnelIcon, TagIcon, XMarkIcon}})
 export default class SideNavFilters extends Vue {
   private router = setup(() => useRouter());
   private store = setup(() => useStore());
