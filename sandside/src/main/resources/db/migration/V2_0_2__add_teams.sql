4d624eedb8020e6f91909682696cc2d143c16551
Marthym <marthym@gmail.com>
2022-12-26 16:56:24 +0100

    feat(sandside): #106 add database and interface for team management
    * Add database migrations
    * Add TeamService interface
    
    refs #106

---
+++ sandside/src/main/resources/db/migration/V2_0_2__add_teams.sql
@@ -0,0 +1,15 @@
+create table TEAMS
+(
+    TEAM_ID         VARCHAR(26) not null primary key,
+    TEAM_NAME       VARCHAR(255),
+    TEAM_CREATED_BY VARCHAR(64) not null,
+    TEAM_CREATED_AT DATETIME
+);
+
+create table TEAMS_MEMBERS
+(
+    TEME_TEAM_ID    VARCHAR(26) not null primary key,
+    TEME_USER_ID    VARCHAR(64) not null,
+    TEME_CREATED_BY VARCHAR(64) not null,
+    TEME_CREATED_AT DATETIME
+);
