e9008697ba1e009f48767ee50a224a65ec190cd8
Marthym <marthym@gmail.com>
2023-01-22 18:04:06 +0100

    feat(sandside): #106 test and fix team management service
    close #106

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/teams/domain/ports/TeamAuthFacade.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/teams/domain/ports/TeamAuthFacade.java
@@ -4,6 +4,8 @@ import fr.ght1pc9kc.baywatch.common.api.model.Entity;
 import fr.ght1pc9kc.baywatch.security.api.model.User;
 import reactor.core.publisher.Mono;
 
+import java.util.Collection;
+
 public interface TeamAuthFacade {
     /**
      * Obtain the connected user.
@@ -15,9 +17,18 @@ public interface TeamAuthFacade {
     /**
      * Grant a {@code MANAGER} permission for et team to the current user
      *
-     * @param authorization The permission
+     * @param authorizations The permissions to grant
      * @return {@code Void} when operation was finished
      * @throws IllegalArgumentException if authorization does not contain entity ID or if the role is unknown
      */
-    Mono<Void> grantAuthorization(String authorization);
+    Mono<Void> grantAuthorization(Collection<String> authorizations);
+
+    /**
+     * Revoke a {@code MANAGER} permission for et team to the current user
+     *
+     * @param authorizations The permissions to revoke
+     * @return {@code Void} when operation was finished
+     * @throws IllegalArgumentException if authorization does not contain entity ID or if the role is unknown
+     */
+    Mono<Void> revokeAuthorization(Collection<String> authorizations);
 }
--- sandside/src/test/java/fr/ght1pc9kc/baywatch/teams/domain/TeamServiceImplTest.java
+++ sandside/src/test/java/fr/ght1pc9kc/baywatch/teams/domain/TeamServiceImplTest.java
@@ -2,10 +2,13 @@ package fr.ght1pc9kc.baywatch.teams.domain;
 
 import fr.ght1pc9kc.baywatch.common.api.model.Entity;
 import fr.ght1pc9kc.baywatch.dsl.tables.Teams;
+import fr.ght1pc9kc.baywatch.dsl.tables.TeamsMembers;
 import fr.ght1pc9kc.baywatch.dsl.tables.records.TeamsRecord;
+import fr.ght1pc9kc.baywatch.security.api.model.Role;
 import fr.ght1pc9kc.baywatch.security.api.model.User;
 import fr.ght1pc9kc.baywatch.teams.api.TeamsService;
 import fr.ght1pc9kc.baywatch.teams.api.exceptions.TeamPermissionDenied;
+import fr.ght1pc9kc.baywatch.teams.domain.model.PendingFor;
 import fr.ght1pc9kc.baywatch.teams.domain.ports.TeamAuthFacade;
 import fr.ght1pc9kc.baywatch.teams.infra.adapters.MembersPersistenceAdapter;
 import fr.ght1pc9kc.baywatch.teams.infra.adapters.TeamPersistenceAdapter;
@@ -36,9 +39,11 @@ import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static fr.ght1pc9kc.baywatch.teams.infra.samples.TeamsRecordSamples.JEDI_TEAM;
+import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Named.named;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -78,6 +83,7 @@ class TeamServiceImplTest {
     void setUp(DSLContext dsl) {
         when(mockAuthFacade.getConnectedUser()).thenReturn(Mono.just(UserSamples.LUKE));
         when(mockAuthFacade.grantAuthorization(any())).thenReturn(Mono.empty().then());
+        when(mockAuthFacade.revokeAuthorization(any())).thenReturn(Mono.empty().then());
 
         Scheduler immediate = Schedulers.immediate();
         TeamsMapper mapper = Mappers.getMapper(TeamsMapper.class);
@@ -124,7 +130,7 @@ class TeamServiceImplTest {
                         () -> Assertions.assertThat(actual.id).isNotBlank(),
                         () -> Assertions.assertThat(actual.createdBy).isEqualTo(UserSamples.OBIWAN.id),
                         () -> Assertions.assertThat(actual.self.name()).isEqualTo("Jedi council team"),
-                        () -> verify(mockAuthFacade).grantAuthorization("MANAGER:" + actual.id)
+                        () -> verify(mockAuthFacade).grantAuthorization(List.of("MANAGER:" + actual.id))
                 )).verifyComplete();
     }
 
@@ -172,5 +178,72 @@ class TeamServiceImplTest {
                         UserSamples.LUKE.id,
                         UserSamples.OBIWAN.id
                 )).verifyComplete();
+    }
+
+    @ParameterizedTest
+    @MethodSource("addMembersProfiles")
+    void should_add_member(Entity<User> user, PendingFor expectedPendingFor) {
+        when(mockAuthFacade.getConnectedUser()).thenReturn(Mono.just(user));
+
+        StepVerifier.create(tested.addMembers(JEDI_TEAM.getTeamId(), List.of(UserSamples.DSIDIOUS.id)).collectList())
+                .assertNext(actual -> assertAll(
+                        () -> Assertions.assertThat(actual).isNotNull().isNotEmpty(),
+                        () -> Assertions.assertThat(actual).extracting(e -> e.self.userId()).containsOnly(
+                                UserSamples.DSIDIOUS.id, UserSamples.LUKE.id, UserSamples.OBIWAN.id),
+                        () -> Assertions.assertThat(requireNonNull(actual).stream()
+                                        .filter(e -> e.self.userId().equals(UserSamples.DSIDIOUS.id))
+                                        .findFirst()).isPresent()
+                                .map(e -> e.self.pending()).contains(expectedPendingFor)
+                )).verifyComplete();
+    }
+
+    static Stream<Arguments> addMembersProfiles() {
+        return Stream.of(
+                arguments(named(UserSamples.DSIDIOUS.self.name, UserSamples.DSIDIOUS), PendingFor.MANAGER),
+                arguments(named(UserSamples.LUKE.self.name, UserSamples.LUKE), PendingFor.USER)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("removeMembersProfiles")
+    void should_remove_member_as(Entity<User> user) {
+        when(mockAuthFacade.getConnectedUser()).thenReturn(Mono.just(user));
+
+        StepVerifier.create(tested.removeMembers(JEDI_TEAM.getTeamId(), List.of(UserSamples.OBIWAN.id)).collectList())
+                .assertNext(actual -> assertAll(
+                        () -> Assertions.assertThat(actual).isNotNull().isNotEmpty(),
+                        () -> Assertions.assertThat(actual).extracting(e -> e.self.userId())
+                                .containsOnly(UserSamples.LUKE.id)
+                )).verifyComplete();
+    }
+
+    static Stream<Arguments> removeMembersProfiles() {
+        return Stream.of(
+                arguments(named(Role.USER.name(), UserSamples.OBIWAN)),
+                arguments(named(Role.MANAGER.name(), UserSamples.LUKE)),
+                arguments(named(Role.ADMIN.name(), UserSamples.YODA))
+        );
+    }
+
+    @Test
+    void should_fail_to_remove_member_as_unauthorized_user() {
+        when(mockAuthFacade.getConnectedUser()).thenReturn(Mono.just(UserSamples.DSIDIOUS));
+
+        StepVerifier.create(tested.removeMembers(JEDI_TEAM.getTeamId(), List.of(UserSamples.OBIWAN.id)).collectList())
+                .verifyError(TeamPermissionDenied.class);
+    }
+
+    @Test
+    void should_delete_team(DSLContext dsl) {
+        StepVerifier.create(tested.delete(List.of(JEDI_TEAM.getTeamId())))
+                .assertNext(actual -> Assertions.assertThat(actual).isEqualTo(JEDI_TEAM.getTeamId()))
+                .verifyComplete();
+
+        Assertions.assertThat(dsl.fetchCount(Teams.TEAMS, Teams.TEAMS.TEAM_ID.eq(JEDI_TEAM.getTeamId())))
+                .as("Ensure the team was deleted from database")
+                .isZero();
+        Assertions.assertThat(dsl.fetchCount(TeamsMembers.TEAMS_MEMBERS, TeamsMembers.TEAMS_MEMBERS.TEME_TEAM_ID.eq(JEDI_TEAM.getTeamId())))
+                .as("Ensure all team members was deleted from database")
+                .isZero();
     }
 }
