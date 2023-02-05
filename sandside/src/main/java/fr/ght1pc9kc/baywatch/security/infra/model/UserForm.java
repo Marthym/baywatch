8e815362620c936afda8e633fc0004e5a4787f9d
Marthym <marthym@gmail.com>
2023-02-05 23:54:44 +0100

    feat(sandside): #151 implement user update for gql

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/security/infra/model/UserForm.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/security/infra/model/UserForm.java
@@ -3,7 +3,6 @@ package fr.ght1pc9kc.baywatch.security.infra.model;
 import fr.ght1pc9kc.baywatch.common.infra.model.CreateValidation;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.Set;
 
@@ -12,5 +11,5 @@ public record UserForm(
         @NotEmpty String name,
         @Email String mail,
         @NotEmpty(groups = CreateValidation.class) String password,
-        @NotNull Set<String> roles) {
+        @NotEmpty(groups = CreateValidation.class) Set<String> roles) {
 }
