472072cb3ca83540da58510c83ae7a971fa936b3
Marthym <marthym@gmail.com>
2023-03-05 20:30:09 +0100

    feat(seaside): #107 implement member status in team member list
    * implement TeamMembers editor
    * add autocomplete for user input
    
    refs #107

--- seaside/src/teams/components/TeamMembersInput.vue
+++ seaside/src/teams/components/TeamMembersInput.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="overflow-x-auto">
+  <div>
     <table class="table table-zebra table-compact">
-      <caption class="label-text text-left mb-2">Manager les membres de l'équipe</caption>
+      <caption class="label-text text-left mb-2">Gérer les membres de l'équipe</caption>
       <thead>
       <tr>
         <th>Utilisateur</th>
-        <th>Rôle</th>
-        <th>Action</th>
+        <th>Status</th>
+        <th class="text-center">Action</th>
       </tr>
       </thead>
       <tbody>
       <tr v-for="(member, index) in members">
-        <td>
-          <TrophyIcon class="h-4 w-4 inline text-amber-300" v-if="member._user.roles.find(r => r === 'MANAGER')"/>
-          {{ member._user.name }}
+        <td v-if="member._id">{{ member._user.login }}</td>
+        <td v-else>
+          <div class="dropdown dropdown-bottom">
+            <input type="text" placeholder="Type here" class="input input-bordered input-xs w-full max-w-xs"
+                   v-model="member._user.login"
+                   @keyup="onUserKeyup"/>
+            <ul tabindex="0" v-if="dropdown.length > 0"
+                class="dropdown-content menu shadow bg-neutral border-primary-content border border-opacity-20 w-full">
+              <li v-for="i in dropdown"><a @click="onUserDropdownClick(index, i)">{{ i.login }}</a></li>
+            </ul>
+          </div>
         </td>
-        <td>
-          {{ member.pending }}
+        <td class="text-center">
+          <component :is="getStatusComponent(member)" class="h-4 w-4 inline text-amber-300"
+                     v-if="getStatusComponent(member) !== undefined"/>
         </td>
         <td>
           <div class="btn-group">
@@ -36,22 +45,26 @@
 
 <script lang="ts">
 import {Options, Prop, Vue} from "vue-property-decorator";
-import {PlusCircleIcon, TrashIcon, TrophyIcon} from "@heroicons/vue/24/outline";
+import {PlusCircleIcon, TrashIcon} from "@heroicons/vue/24/outline";
+import {ClockIcon, TrophyIcon, UserPlusIcon} from "@heroicons/vue/20/solid";
 import {Member} from "@/teams/model/Member.type";
-import {teamMemberList} from "@/teams/services/TeamMembers.service";
+import {teamMemberAvailable, teamMemberList} from "@/teams/services/TeamMembers.service";
+import {MemberPending} from "@/teams/model/MemberPending.enum";
+import {User} from "@/teams/model/User.type";
 
 const SUBMIT_EVENT: string = 'submit';
 const UPDATE_EVENT: string = 'update:modelValue';
 
 @Options({
   name: 'TeamMembersInput',
-  components: {PlusCircleIcon, TrashIcon, TrophyIcon},
+  components: {PlusCircleIcon, TrashIcon, TrophyIcon, UserPlusIcon, ClockIcon},
   emits: [UPDATE_EVENT, SUBMIT_EVENT],
 })
 export default class TeamMembersInput extends Vue {
   @Prop()
   private teamId!: string;
   private members: Member[] = [];
+  private dropdown: User[] = [];
 
   mounted(): void {
     teamMemberList(this.teamId).subscribe({
@@ -59,8 +72,36 @@ export default class TeamMembersInput extends Vue {
     })
   }
 
-  private onAddRole(): void {
+  private getStatusComponent(member: Member): string | undefined {
+    switch (member.pending) {
+      case MemberPending.MANAGER:
+        return 'UserPlus';
+      case MemberPending.USER:
+        return 'ClockIcon';
+      case MemberPending.NONE:
+      default:
+        if (member._user.roles.find(r => r === 'MANAGER')) {
+          return 'TrophyIcon';
+        }
+        return undefined;
+    }
+  }
 
+  private onAddRole(): void {
+    this.members.push({_user: {}, pending: MemberPending.USER} as Member);
+  }
+
+  private onUserKeyup(event: KeyboardEvent): void {
+    if (event.target && (event.target as HTMLInputElement).value.length > 1) {
+      teamMemberAvailable((event.target as HTMLInputElement).value).subscribe({
+        next: res => this.dropdown.splice(0, this.dropdown.length, ...res),
+      });
+    }
+  }
+
+  private onUserDropdownClick(idx: number, u: User): void {
+    this.members[idx]._user = u;
+    this.dropdown.splice(0, this.dropdown.length);
   }
 
   private onRemoveRole(index: number): void {
--- seaside/src/teams/services/TeamMembers.service.ts
+++ seaside/src/teams/services/TeamMembers.service.ts
@@ -2,6 +2,7 @@ import {Observable} from "rxjs";
 import {send} from "@/common/services/GraphQLClient";
 import {map, take} from "rxjs/operators";
 import {Member} from "@/teams/model/Member.type";
+import {User} from "@/teams/model/User.type";
 
 /**
  * Search for Teams
@@ -9,13 +10,33 @@ import {Member} from "@/teams/model/Member.type";
 const TEAM_MEMBERS_LIST_REQUEST = `#graphql
 query ListTeamMembers($teamId: ID){
     teamMembersList(teamId: $teamId) {
-        _id _user {_id name roles} pending
+        _id _user {_id login roles} pending
     }
 }`
 
 export function teamMemberList(teamId: string): Observable<Member[]> {
     return send<{ teamMembersList: Member[] }>(TEAM_MEMBERS_LIST_REQUEST, {teamId: teamId}).pipe(
         map(data => data.data.teamMembersList),
+        take(1),
+    );
+}
+
+const USER_FILTER_REQUEST = `#graphql
+query FilterAvailableUsers($_p: Int, $_pp: Int, $term: String){
+    userSearch(_p: $_p, _pp: $_pp, login: $term) {
+        entities {
+            _id login
+        }
+    }
+}`
+
+export function teamMemberAvailable(term: string): Observable<User[]> {
+    const vars = {
+        term: `^${term}`,
+        _pp: 10
+    };
+    return send<{ userSearch: { entities: User[] } }>(USER_FILTER_REQUEST, vars).pipe(
+        map(data => data.data.userSearch.entities),
         take(1),
     );
 }
