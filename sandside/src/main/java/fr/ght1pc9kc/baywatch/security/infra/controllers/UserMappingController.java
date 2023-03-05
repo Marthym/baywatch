ae7ac8191a0aeba4f9f2a2bbd6bd35de9a2b68a5
Marthym <marthym@gmail.com>
2023-03-05 17:49:13 +0100

    feat(sandside): #107 refactor UserGqlController to allow retrieving team members data

---
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/security/infra/controllers/UserMappingController.java
@@ -0,0 +1,73 @@
+package fr.ght1pc9kc.baywatch.security.infra.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
+import fr.ght1pc9kc.baywatch.common.api.model.Entity;
+import fr.ght1pc9kc.baywatch.security.api.UserService;
+import fr.ght1pc9kc.baywatch.security.api.model.Permission;
+import fr.ght1pc9kc.baywatch.security.api.model.User;
+import fr.ght1pc9kc.juery.api.Criteria;
+import fr.ght1pc9kc.juery.api.PageRequest;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static fr.ght1pc9kc.baywatch.common.api.model.EntitiesProperties.ID;
+import static fr.ght1pc9kc.baywatch.common.api.model.EntitiesProperties.ROLES;
+
+@Controller
+@RequiredArgsConstructor
+public class UserMappingController {
+    private final UserService userService;
+    private final ObjectMapper mapper;
+
+    @SchemaMapping(typeName = "Team", value = "_createdBy")
+    public Mono<Map<String, Object>> createdBy(Map<String, Object> user) {
+        MapType gqlType = mapper.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
+        return userService.get(user.get("_createdBy").toString()).map(e -> mapper.convertValue(e, gqlType));
+    }
+
+    @SchemaMapping(typeName = "Team", value = "_managers")
+    public Flux<Map<String, Object>> managers(Map<String, Object> team) {
+        MapType gqlType = mapper.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
+        return userService.list(PageRequest.all(Criteria.property(ROLES).eq(Permission.manager(team.get("_id").toString()))))
+                .map(e -> mapper.convertValue(e, gqlType));
+    }
+
+    @BatchMapping(typeName = "TeamMember", field = "_user")
+    public Mono<Map<Map<String, Object>, Map<String, Object>>> teamMembers(List<Map<String, Object>> members) {
+        MapType gqlType = mapper.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
+        List<String> userIds = members.stream().map(m -> m.get("userId").toString()).toList();
+        return userService.list(PageRequest.all(Criteria.property(ID).in(userIds))).collectList()
+                .map(users -> users.stream().collect(Collectors.toUnmodifiableMap(e -> e.id, Function.identity())))
+                .map(users -> members.stream().collect(Collectors.toUnmodifiableMap(
+                        Function.identity(),
+                        m -> {
+                            Entity<User> teamMember = users.get(m.getOrDefault("userId", "").toString());
+                            if (teamMember == null) {
+                                return mapper.convertValue(Entity.identify(Entity.NO_ONE, User.ANONYMOUS), gqlType);
+                            }
+                            teamMember = Entity.identify(teamMember.id, filterRoles(teamMember.self, m.get("_id").toString()));
+                            return mapper.convertValue(teamMember, gqlType);
+                        }
+                )));
+    }
+
+    private User filterRoles(@NotNull User user, @NotNull String entityId) {
+        return user.toBuilder()
+                .clearRoles()
+                .roles(user.roles.stream()
+                        .filter(p -> p.entity().map(id -> id.equals(entityId)).orElse(false))
+                        .map(Permission::role)
+                        .toList()).build();
+    }
+}
---
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/teams/infra/controllers/TeamMembersController.java
@@ -0,0 +1,29 @@
+package fr.ght1pc9kc.baywatch.teams.infra.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
+import fr.ght1pc9kc.baywatch.teams.api.TeamsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('USER', 'MANAGER', 'ADMIN')")
+public class TeamMembersController {
+
+    private final TeamsService teamsService;
+    private final ObjectMapper mapper;
+
+    @QueryMapping
+    public Flux<Map<String, Object>> teamMembersList(@Argument("teamId") String teamId) {
+        MapType gqlType = mapper.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
+        return teamsService.members(teamId)
+                .map(e -> mapper.convertValue(e, gqlType));
+    }
+}
