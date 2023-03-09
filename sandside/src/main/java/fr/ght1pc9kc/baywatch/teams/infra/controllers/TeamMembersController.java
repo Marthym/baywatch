c9e2473f7e971d179e746a341efae86f5d6ad3c1
Marthym <marthym@gmail.com>
2023-03-09 23:08:46 +0100

    fix(sandside): #107 add controller for team members add and delete
    and fix some lint warning

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/teams/infra/controllers/TeamMembersController.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/teams/infra/controllers/TeamMembersController.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.databind.type.MapType;
 import fr.ght1pc9kc.baywatch.teams.api.TeamsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -24,6 +26,20 @@ public class TeamMembersController {
     public Flux<Map<String, Object>> teamMembersList(@Argument("teamId") String teamId) {
         MapType gqlType = mapper.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
         return teamsService.members(teamId)
+                .map(e -> mapper.convertValue(e, gqlType));
+    }
+
+    @MutationMapping
+    public Flux<Map<String, Object>> teamMembersAdd(@Argument("_id") String teamId, @Argument("membersIds") List<String> membersIds) {
+        MapType gqlType = mapper.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
+        return teamsService.addMembers(teamId, membersIds)
+                .map(e -> mapper.convertValue(e, gqlType));
+    }
+
+    @MutationMapping
+    public Flux<Map<String, Object>> teamMembersDelete(@Argument("_id") String teamId, @Argument("membersIds") List<String> membersIds) {
+        MapType gqlType = mapper.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
+        return teamsService.removeMembers(teamId, membersIds)
                 .map(e -> mapper.convertValue(e, gqlType));
     }
 }
