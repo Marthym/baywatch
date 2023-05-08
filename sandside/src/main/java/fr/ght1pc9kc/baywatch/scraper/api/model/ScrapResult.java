14067d1687d51240330be612f3c09934b23e2dfb
Marthym <marthym@gmail.com>
2023-05-08 17:19:17 +0200

    chore(sandside): #157 refactor and extract scraper launcher from the service
    ScraperService only do scraping and ScraperTaskScheduler manage the scrap launch
    this allow to add metrics out of the domain for scraping

---
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/scraper/api/model/ScrapResult.java
@@ -0,0 +1,7 @@
+package fr.ght1pc9kc.baywatch.scraper.api.model;
+
+public record ScrapResult(
+        long inserted,
+        long error
+) {
+}
---
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/scraper/api/model/ScrapingException.java
@@ -0,0 +1,7 @@
+package fr.ght1pc9kc.baywatch.scraper.api.model;
+
+public class ScrapingException extends RuntimeException {
+    public ScrapingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
