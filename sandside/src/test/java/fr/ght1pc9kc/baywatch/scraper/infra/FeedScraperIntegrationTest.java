7dd1749fe14f8b763ab1288d0c2cb2937a34c364
Marthym <marthym@gmail.com>
2023-05-08 23:30:06 +0200

    feat(sandside): #157 add metrics to scraping

--- sandside/src/test/java/fr/ght1pc9kc/baywatch/scraper/infra/FeedScraperIntegrationTest.java
+++ sandside/src/test/java/fr/ght1pc9kc/baywatch/scraper/infra/FeedScraperIntegrationTest.java
@@ -25,6 +25,8 @@ import fr.ght1pc9kc.baywatch.techwatch.api.model.News;
 import fr.ght1pc9kc.baywatch.tests.samples.UserSamples;
 import fr.ght1pc9kc.scraphead.core.HeadScrapers;
 import fr.ght1pc9kc.scraphead.netty.http.NettyScrapClient;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import io.micrometer.observation.ObservationRegistry;
 import org.assertj.core.api.Assertions;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeAll;
@@ -165,7 +167,7 @@ class FeedScraperIntegrationTest {
                         scraperConfiguration.getScraperScheduler())
         );
 
-        tested = new ScraperTaskScheduler(scraperService, scraperProperties);
+        tested = new ScraperTaskScheduler(scraperService, scraperProperties, ObservationRegistry.NOOP, new SimpleMeterRegistry());
     }
 
     @ParameterizedTest
