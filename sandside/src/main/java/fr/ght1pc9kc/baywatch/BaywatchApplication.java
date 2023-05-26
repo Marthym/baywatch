d8985eb1e4595aa3e3ed7f30d3f258f26cf01551
Marthym <marthym@gmail.com>
2023-05-26 16:45:55 +0200

    chore(sandside): #157 fix sonar lint errors

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/BaywatchApplication.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/BaywatchApplication.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 
 @Slf4j
 @SpringBootApplication
