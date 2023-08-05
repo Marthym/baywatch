5fd9cd917ae63ddf3f1446a566404eac1a5dda33
Marthym <marthym@gmail.com>
2023-08-05 20:28:49 +0200

    feat(sandside): #170 improve password check message

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/security/api/model/PasswordEvaluation.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/security/api/model/PasswordEvaluation.java
@@ -3,6 +3,6 @@ package fr.ght1pc9kc.baywatch.security.api.model;
 public record PasswordEvaluation(
         boolean isSecure,
         double entropy,
-        String timeToCrack
+        String message
 ) {
 }
