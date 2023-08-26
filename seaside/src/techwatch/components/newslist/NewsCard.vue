68fcf964f6a43618886a2cde5f1237b1f8e3b8b1
Marthym <marthym@gmail.com>
2023-08-26 17:45:53 +0200

    feat(seaside): #176 add read on click for news card
    close #176

--- seaside/src/techwatch/components/newslist/NewsCard.vue
+++ seaside/src/techwatch/components/newslist/NewsCard.vue
@@ -22,7 +22,8 @@
     <div :class="{ 'm-6': card.isActive, 'm-4': !card.isActive}" class="flex-grow">
       <div class="flex flex-col h-full overflow-hidden">
         <a class="font-semibold text-xl" target="_blank" :href="card.data.link" :title="card.data.link"
-           v-html="card.data.title"></a>
+           @auxclick="$emit('clickTitle')"
+           @click="$emit('clickTitle')" v-html="card.data.title"></a>
         <span v-html="card.data.description" class="mt-2 text-base flex-grow max-h-80 overflow-hidden"></span>
 
         <div class="flex flex-row flex-wrap-reverse lg:justify-end text-xs mt-2">
@@ -30,7 +31,8 @@
           <span class="grow"></span>
           <span class="italic self-start lg:block">{{ publication }}</span>
           <div class="text-right lg:whitespace-nowrap order-last basis-full">
-            <button class="badge badge-neutral badge-sm m-px rounded lg:whitespace-nowrap order-last basis-full" @click.stop="$emit('addFilter', {type: 'feed', entity: f})"
+            <button class="badge badge-neutral badge-sm m-px rounded lg:whitespace-nowrap order-last basis-full"
+                    @click.stop="$emit('addFilter', {type: 'feed', entity: f})"
                     v-for="f in card.data.feeds">{{ f.name }}
             </button>
           </div>
@@ -41,13 +43,13 @@
 </template>
 
 <script lang="ts">
-import {Component, Prop, Vue} from 'vue-facing-decorator';
-import {NewsView} from "@/techwatch/components/newslist/model/NewsView";
-import {ImgHTMLAttributes} from "vue";
+import { Component, Prop, Vue } from 'vue-facing-decorator';
+import { NewsView } from '@/techwatch/components/newslist/model/NewsView';
+import { ImgHTMLAttributes } from 'vue';
 
 const EMPTY_IMAGE_DATA: string = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
 
-@Component({name: 'NewsCard', emits: ['activate', 'addFilter']})
+@Component({ name: 'NewsCard', emits: ['activate', 'addFilter', 'clickTitle'] })
 export default class NewsCard extends Vue {
   @Prop() card: NewsView;
 
@@ -59,7 +61,7 @@ export default class NewsCard extends Vue {
     if (this.card) {
       return new Date(this.card.data.publication).toLocaleDateString(navigator.languages, {
         timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-        year: 'numeric', month: 'short', day: '2-digit', hour: "2-digit", minute: "2-digit"
+        year: 'numeric', month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit',
       });
     } else {
       return undefined;
