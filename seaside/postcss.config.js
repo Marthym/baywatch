1334a5f331c2ef7188a19cb6624e003357e4f217
Marthym <marthym@gmail.com>
2023-11-18 23:38:02 +0100

    chore(seaside): #185 upgrade all dependencies
    refs #185

--- seaside/postcss.config.js
+++ seaside/postcss.config.js
@@ -1,7 +1,7 @@
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss');
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
 
-module.exports = {
+export default {
     plugins: [
         tailwindcss,
         autoprefixer,
