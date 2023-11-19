ca7e380502ec22822423e70d5a4a068e07b55795
Marthym <marthym@gmail.com>
2023-11-19 10:37:26 +0100

    chore(seaside): #185 fix regexp backtracking vulnerability

---
+++ seaside/src/common/services/RegexPattern.ts
@@ -0,0 +1,3 @@
+export const URL_PATTERN = /^(((https?):\/\/)(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)([).!';/?:,][^\S\n\r])?$/;
+export const MAIL_PATTERN = /^[a-zA-Z0-9_+&*-]+(?:\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,7}$/;
+export const ULID_PATTERN = /^[A-Z]*(:[A-Z]{2}[0-7][0-9A-HJKMNP-TV-Z]{25})?$/;
