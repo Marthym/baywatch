3c9a71a700559241b30ac590d9e136a54da3ee1e
Marthym <marthym@gmail.com>
2023-11-20 20:42:48 +0100

    fix(sandside): #187 avoid change image on external link
    close #187

--- sandside/src/test/java/fr/ght1pc9kc/baywatch/scraper/domain/filters/RedditNewsFilterTest.java
+++ sandside/src/test/java/fr/ght1pc9kc/baywatch/scraper/domain/filters/RedditNewsFilterTest.java
@@ -10,37 +10,64 @@ import java.net.URI;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RedditNewsFilterTest {
-    private static final RawNews RAW = RawNews.builder()
+    private static final RawNews RAW_EXTERNAL = RawNews.builder()
             .id("0")
             .link(URI.create("http://www.reddit.com/u/okenobi"))
             .title("Start with this title")
-            .description("""
-                    &lt;!-- SC_OFF --&gt;&lt;div class=&quot;md&quot;&gt;&lt;p&gt;These three posts describe
-                    how to deploy Jakarta EE 9 applications to &lt;a href=&quot;https://hantsy.medium.com/deploying-jakarta-ee-9-applications-to-wildfly-e271d1403b2c?source=friends_link&amp;amp;sk=c20ec9f09891287d698e29d7368cab8d&quot;&gt;WildFly&lt;/a&gt;,
-                    &lt;a href=&quot;https://hantsy.medium.com/deploying-jakarta-ee-9-applications-to-apache-tomee-e1a9b9399d9b?source=friends_link&amp;amp;sk=734db0a3c6e205039c6ad2a392235880&quot;&gt;Apache
-                    TomEE&lt;/a&gt; and &lt;a href=&quot;https://hantsy.medium.com/deploying-jakarta-ee-9-applications-to-open-liberty-dac4529f48c6?source=friends_link&amp;amp;sk=47a141c9ad845826e6c822068f37881b&quot;&gt;Open
-                    Liberty&lt;/a&gt;.&lt;/p&gt; &lt;/div&gt;&lt;!-- SC_ON --&gt; &amp;#32; submitted by &amp;#32; &lt;a href=&quot;https://www.reddit.com/user/congolomera&quot;&gt;
-                    /u/congolomera &lt;/a&gt; &lt;br/&gt; &lt;span&gt;&lt;a href=&quot;https://www.reddit.com/r/java/comments/k3hmfl/deploying_jakarta_ee_9_applications_to_wildfly/&quot;&gt;[link]&lt;/a&gt;&lt;/span&gt;
-                    &amp;#32; &lt;span&gt;&lt;a href=&quot;https://www.reddit.com/r/java/comments/k3hmfl/deploying_jakarta_ee_9_applications_to_wildfly/&quot;&gt;[commentaires]&lt;/a&gt;&lt;/span&gt;
-                    """).build();
+            .description("&amp;#32; submitted by &amp;#32; &lt;a href=&quot;https://www.reddit.com/user/mohan-thatguy&quot;&gt; " +
+                    "/u/mohan-thatguy &lt;/a&gt; &lt;br/&gt; &lt;span&gt;&lt;a " +
+                    "href=&quot;https://proxiesapi.com/articles/the-ultimate-jsoup-cheatsheet-in-java&quot;&gt;[link]&lt;/a&gt;&lt;/span&gt; " +
+                    "&amp;#32; &lt;span&gt;&lt;a " +
+                    "href=&quot;https://www.reddit.com/r/java/comments/17zqw6y/the_complete_jsoup_cheatsheet_in_java/&quot;&gt;" +
+                    "[comments]&lt;/a&gt;&lt;/span&gt; ").build();
+
+    private static final RawNews RAW_INTERNAL = RawNews.builder()
+            .id("0")
+            .link(URI.create("https://www.reddit.com/r/java/comments/17zn7tt/regular_classes_with_canonical_constructor/"))
+            .title("Start with this title")
+            .description("&lt;!-- SC_OFF --&gt;&lt;div class=&quot;md&quot;&gt;&lt;p&gt;" +
+                    "Does it make sense to wish regular classes to have canonical constructor in the future?&lt;/p&gt; " +
+                    "&lt;pre&gt;&lt;code&gt;@RestController @HttpExchange public UserController(UserService userService) " +
+                    "{ … } &lt;/code&gt;&lt;/pre&gt; &lt;p&gt;It’s shorter. The difference from &lt;code&gt;records&lt;" +
+                    "/code&gt;s is no implicit accessors.&lt;/p&gt; &lt;p&gt;Except for multiple syntaxes/doing it in " +
+                    "multiple ways, what would be the cons?&lt;/p&gt; &lt;p&gt;What is the probability that it would be " +
+                    "considered by our great Java language designers?&lt;/p&gt; &lt;/div&gt;&lt;!-- SC_ON --&gt; &amp;#32; " +
+                    "submitted by &amp;#32; &lt;a href=&quot;https://www.reddit.com/user/jvjupiter&quot;&gt; /u/jvjupiter " +
+                    "&lt;/a&gt; &lt;br/&gt; &lt;span&gt;&lt;a href=&quot;" +
+                    "https://www.reddit.com/r/java/comments/17zn7tt/regular_classes_with_canonical_constructor/&quot;" +
+                    "&gt;[link]&lt;/a&gt;&lt;/span&gt; &amp;#32; &lt;span&gt;&lt;a href=&quot;" +
+                    "https://www.reddit.com/r/java/comments/17zn7tt/regular_classes_with_canonical_constructor/" +
+                    "&quot;&gt;[comments]&lt;/a&gt;&lt;/span&gt;").build();
 
     private final RedditNewsFilter tested = new RedditNewsFilter("https://www.jide.com/reddit.png");
 
     @Test
-    void should_filter_reddit() {
-        StepVerifier.create(tested.filter(RAW))
+    void should_filter_external_reddit() {
+        StepVerifier.create(tested.filter(RAW_EXTERNAL))
                 .assertNext(actual -> Assertions.assertAll(
-                        () -> assertThat(actual.id()).isEqualTo("c74cb819fe7a596814406c9ec164dfa5d502fba8659ec13e634a33d1ae7cbd56"),
-                        () -> assertThat(actual.title()).isEqualTo(RAW.title()),
-                        () -> assertThat(actual.description()).isEqualTo(RAW.description()),
+                        () -> assertThat(actual.id()).isEqualTo("0c1d1c395b81ed7ee98f25b2a3ac8b0719a256593eb072f8fa5bd926d2413a1b"),
+                        () -> assertThat(actual.title()).isEqualTo(RAW_EXTERNAL.title()),
+                        () -> assertThat(actual.description()).isEqualTo(RAW_EXTERNAL.description()),
+                        () -> assertThat(actual.image()).isNull(),
+                        () -> assertThat(actual.link()).isEqualTo(URI.create("https://proxiesapi.com/articles/the-ultimate-jsoup-cheatsheet-in-java"))
+                )).verifyComplete();
+    }
+
+    @Test
+    void should_filter_internal_reddit() {
+        StepVerifier.create(tested.filter(RAW_INTERNAL))
+                .assertNext(actual -> Assertions.assertAll(
+                        () -> assertThat(actual.id()).isEqualTo(RAW_INTERNAL.id()),
+                        () -> assertThat(actual.title()).isEqualTo(RAW_INTERNAL.title()),
+                        () -> assertThat(actual.description()).isEqualTo(RAW_INTERNAL.description()),
                         () -> assertThat(actual.image()).hasToString("https://www.jide.com/reddit.png"),
-                        () -> assertThat(actual.link()).isEqualTo(URI.create("https://www.reddit.com/r/java/comments/k3hmfl/deploying_jakarta_ee_9_applications_to_wildfly/"))
+                        () -> assertThat(actual.link()).isEqualTo(RAW_INTERNAL.link())
                 )).verifyComplete();
     }
 
     @Test
     void should_filter_reddit_without_reddit() {
-        RawNews rawNewsNotReddit = RAW.withLink(URI.create("https://www.jedi.com/not/reddit"));
+        RawNews rawNewsNotReddit = RAW_EXTERNAL.withLink(URI.create("https://www.jedi.com/not/reddit"));
         StepVerifier.create(tested.filter(rawNewsNotReddit))
                 .assertNext(actual -> assertThat(actual).isSameAs(rawNewsNotReddit)).verifyComplete();
     }
