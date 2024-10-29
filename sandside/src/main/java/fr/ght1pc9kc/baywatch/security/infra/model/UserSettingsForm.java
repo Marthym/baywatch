e38377fd800c01539910c9ff557be6ad09c4d55f
Marthym <marthym@gmail.com>
2024-10-29 23:22:34 +0100

    feat(sandside): #214 add newsView parameter

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/security/infra/model/UserSettingsForm.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/security/infra/model/UserSettingsForm.java
@@ -1,9 +1,12 @@
 package fr.ght1pc9kc.baywatch.security.infra.model;
 
+import fr.ght1pc9kc.baywatch.security.api.model.NewsViewType;
+
 import java.util.Locale;
 
 public record UserSettingsForm(
         Locale preferredLocale,
-        boolean autoread
+        boolean autoread,
+        NewsViewType newsView
 ) {
 }
