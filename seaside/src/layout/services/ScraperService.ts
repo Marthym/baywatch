92d1f7e7babd6e6a34998210886af6321e0e13df
Marthym <marthym@gmail.com>
2025-07-30 17:06:12 +0200

    fix(seaside): #241 fix scraper service

--- seaside/src/layout/services/ScraperService.ts
+++ seaside/src/layout/services/ScraperService.ts
@@ -3,24 +3,20 @@ import { map, take } from 'rxjs/operators';
 import { send } from '@/common/services/GraphQLClient';
 import { URL_PATTERN } from '@/common/services/RegexPattern';
 
-export class ScraperService {
-    private static readonly SCRAP_SINGLE_NEWS_REQUEST = `#graphql
-    mutation ScrapSingleNews($newsLink: URI!) {
-        scrapSimpleNews(uri: $newsLink)
-    }`;
+const SCRAP_SINGLE_NEWS_REQUEST = `#graphql
+mutation ScrapSingleNews($newsLink: URI!) {
+    scrapSimpleNews(uri: $newsLink)
+}`;
 
-    importStandaloneNews(link?: string): Observable<void> {
-        if (link === undefined) {
-            return throwError(() => new Error('Link is mandatory !'));
-        } else if (!URL_PATTERN.test(link)) {
-            return throwError(() => new Error('Argument link must be a valid URL !'));
-        }
-
-        return send(ScraperService.SCRAP_SINGLE_NEWS_REQUEST, { newsLink: link }).pipe(
-            take(1),
-            map(() => undefined),
-        );
+export function scraperImportStandaloneNews(link?: string): Observable<void> {
+    if (link === undefined) {
+        return throwError(() => new Error('Link is mandatory !'));
+    } else if (!URL_PATTERN.test(link)) {
+        return throwError(() => new Error('Argument link must be a valid URL !'));
     }
-}
 
-export default new ScraperService();
+    return send(SCRAP_SINGLE_NEWS_REQUEST, { newsLink: link }).pipe(
+        take(1),
+        map(() => undefined),
+    );
+}
