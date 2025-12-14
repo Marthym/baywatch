d787812edcfb116239736bbec7d428b59d3dc115
Marthym <marthym@gmail.com>
2025-12-14 19:55:22 +0100

    fix(sandside): #250 fix sonar errors

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/common/infra/validators/NullOrNotBlankValidator.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/common/infra/validators/NullOrNotBlankValidator.java
@@ -3,13 +3,11 @@ package fr.ght1pc9kc.baywatch.common.infra.validators;
 import fr.ght1pc9kc.baywatch.common.api.constraints.NullOrNotBlank;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.springframework.context.i18n.LocaleContextHolder;
 
 public final class NullOrNotBlankValidator implements ConstraintValidator<NullOrNotBlank, CharSequence> {
 
     @Override
     public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
-        System.out.println("Locale = " + LocaleContextHolder.getLocale());
         if (value == null) return true;
         return !value.toString().trim().isEmpty();
     }
