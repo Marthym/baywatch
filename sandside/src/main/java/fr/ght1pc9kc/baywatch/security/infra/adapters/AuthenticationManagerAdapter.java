3103da7d4f2c2ebbf6e4dddace8eb0a1803cbeea
Marthym <marthym@gmail.com>
2026-01-16 23:05:03 +0100

    fix(sandside): #251 reject login on unvalidated user

--- sandside/src/main/java/fr/ght1pc9kc/baywatch/security/infra/adapters/AuthenticationManagerAdapter.java
+++ sandside/src/main/java/fr/ght1pc9kc/baywatch/security/infra/adapters/AuthenticationManagerAdapter.java
@@ -4,8 +4,9 @@ import fr.ght1pc9kc.baywatch.security.api.model.AuthenticationRequest;
 import fr.ght1pc9kc.baywatch.security.api.model.BaywatchAuthentication;
 import fr.ght1pc9kc.baywatch.security.domain.ports.AuthenticationManagerPort;
 import fr.ght1pc9kc.baywatch.security.infra.model.BaywatchUserDetails;
+import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,13 +16,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import static java.util.Objects.isNull;
+
 @Component
 public class AuthenticationManagerAdapter implements AuthenticationManagerPort, ReactiveAuthenticationManager {
     private final ReactiveAuthenticationManager delegate;
 
     @Autowired
     public AuthenticationManagerAdapter(
-            @Qualifier("Baywatch") UserServiceAdapter userDetailsService,
+            UserServiceAdapter userDetailsService,
             PasswordEncoder passwordEncoder) {
         UserDetailsRepositoryReactiveAuthenticationManager authManager =
                 new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
@@ -32,16 +35,22 @@ public class AuthenticationManagerAdapter implements AuthenticationManagerPort, 
     @Override
     public Mono<BaywatchAuthentication> authenticate(AuthenticationRequest request) {
         return delegate.authenticate(new UsernamePasswordAuthenticationToken(request.username(), request.password()))
-                .map(auth -> new BaywatchAuthentication(
-                        ((BaywatchUserDetails) auth.getPrincipal()).entity(),
-                        null,
-                        request.rememberMe(),
-                        AuthorityUtils.authorityListToSet(auth.getAuthorities())
-                ));
+                .handle((auth, sink) -> {
+                    if (isNull(auth.getPrincipal()) || auth.getAuthorities().isEmpty()) {
+                        sink.error(new BadCredentialsException("User has no authorities"));
+                    } else {
+                        sink.next(new BaywatchAuthentication(
+                                ((BaywatchUserDetails) auth.getPrincipal()).entity(),
+                                null,
+                                request.rememberMe(),
+                                AuthorityUtils.authorityListToSet(auth.getAuthorities())
+                        ));
+                    }
+                });
     }
 
     @Override
-    public Mono<Authentication> authenticate(Authentication authentication) {
+    public @NonNull Mono<Authentication> authenticate(@NonNull Authentication authentication) {
         return delegate.authenticate(authentication);
     }
 }
--- sandside/src/test/java/fr/ght1pc9kc/baywatch/security/infra/adapters/AuthenticationManagerAdapterTest.java
+++ sandside/src/test/java/fr/ght1pc9kc/baywatch/security/infra/adapters/AuthenticationManagerAdapterTest.java
@@ -2,10 +2,12 @@ package fr.ght1pc9kc.baywatch.security.infra.adapters;
 
 import fr.ght1pc9kc.baywatch.security.api.model.AuthenticationRequest;
 import fr.ght1pc9kc.baywatch.security.api.model.BaywatchAuthentication;
+import fr.ght1pc9kc.baywatch.security.api.model.User;
 import fr.ght1pc9kc.baywatch.security.infra.model.BaywatchUserDetails;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import reactor.core.publisher.Mono;
@@ -19,11 +21,11 @@ import static org.mockito.Mockito.mock;
 class AuthenticationManagerAdapterTest {
 
     private AuthenticationManagerAdapter tested;
+    private final UserServiceAdapter mockUserService = mock(UserServiceAdapter.class);
 
     @BeforeEach
     @SuppressWarnings("deprecation")
     void setUp() {
-        UserServiceAdapter mockUserService = mock(UserServiceAdapter.class);
         doReturn(Mono.just(new BaywatchUserDetails(OBIWAN))).when(mockUserService).findByUsername(anyString());
         tested = new AuthenticationManagerAdapter(mockUserService, NoOpPasswordEncoder.getInstance());
     }
@@ -44,5 +46,12 @@ class AuthenticationManagerAdapterTest {
                     soft.assertThat(actual).isInstanceOf(BaywatchAuthentication.class);
                     soft.assertThat(actual.user()).isEqualTo(OBIWAN);
                 })).verifyComplete();
+    }
+
+    @Test
+    void should_authenticate_unvalidated_user() {
+        doReturn(Mono.just(new BaywatchUserDetails(OBIWAN.convert(User::withRoles)))).when(mockUserService).findByUsername(anyString());
+        StepVerifier.create(tested.authenticate(new AuthenticationRequest(OBIWAN.self().login(), "nawibo", false)))
+                .verifyError(BadCredentialsException.class);
     }
 }
